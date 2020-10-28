65f2839f073120d62d88ac52094ad7b68da2b24e
Mark Dunbavan <mdunbavan@gmail.com>
2020-10-28 10:27:51 +0000

    fix errors

--- express/server.js
+++ express/server.js
@@ -5,36 +5,53 @@ const serverless = require("serverless-http");
 const app = express();
 const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
+const axios = require("axios");
 var async = require("express-async-await");
 
 const router = express.Router();
 router.get("/api", async function(req, res, next) {
   res.setHeader("Content-Type", "application/json");
-  var requestOptions = {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      Authorization:
-        "Basic Y2VjNTZhY2UtNTI3ZC00NjIzLWE2ZmEtYmNhMGU1ZWI1ODkwOg==",
-      "Access-Control-Allow-Origin": "*"
-    },
-    redirect: "follow"
-  };
+
+  // var requestOptions = {
+  //   method: "POST",
+  //   headers: {
+  //     Accept: "application/json",
+  //     Authorization:
+  //       "Basic Y2VjNTZhY2UtNTI3ZC00NjIzLWE2ZmEtYmNhMGU1ZWI1ODkwOg==",
+  //     "Access-Control-Allow-Origin": "*"
+  //   },
+  //   redirect: "follow"
+  // };
 
   try {
-    let result = await getApi(requestOptions);
-    res.status(200).json(result);
+    let result = getApi();
+    result.then(function(result) {
+      res.status(200).json(result); // "Some User token"
+    });
+    // let result = await getApi();
+    // res.status(200).json(result);
   } catch (err) {
     return res.status(500).json(err);
   }
 
-  function getApi(requestOptions) {
-    return fetch(
-      "http://www.reed.co.uk/api/1.0/search?employerId=488552",
-      requestOptions
-    ).then(res => {
-      return res.json();
-    });
+  async function getApi() {
+    const promise = await axios
+      .post(
+        "http://www.reed.co.uk/api/1.0/search?employerId=488552",
+        {},
+        {
+          headers: {
+            Authorization:
+              "Basic Y2VjNTZhY2UtNTI3ZC00NjIzLWE2ZmEtYmNhMGU1ZWI1ODkwOg==",
+            Accept: "application/json"
+          }
+        }
+      )
+      .then(response => response.data);
+
+    const dataPromise = promise;
+
+    return dataPromise;
   }
 });
 router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
@@ -42,7 +59,6 @@ router.post("/", (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use("/.netlify/functions/server", router); // path must route to lambda
-app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
 module.exports = app;
 module.exports.handler = serverless(app);
